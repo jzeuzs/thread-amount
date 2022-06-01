6dd7ba38336624895860e17a21cbef3d3ef9148c
Tomio <mail@tomio.codes>
2022-06-01 08:29:34 +0800

    fix: read `proc/[PID]/status` manually

--- src/unix.rs
+++ src/unix.rs
@@ -1,10 +1,19 @@
 use std::num::NonZeroUsize;
-
-use procfs::process::Process;
+use std::{fs, process};
 
 pub(crate) fn thread_amount() -> Option<NonZeroUsize> {
-    let process = Process::myself().expect("Failed getting process info");
-    let status = process.status().expect("Failed getting status");
+    let mut amount: Option<NonZeroUsize> = None;
+    let status = fs::read_to_string(format!("/proc/{}/status", process::id()))
+        .expect("Failed reading status");
 
-    NonZeroUsize::new(status.threads as usize)
+    for line in status.split('\n') {
+        if line.to_lowercase().starts_with("threads:") {
+            let new_line = line.replace(' ', "").to_lowercase().replace("threads:", "");
+
+            amount =
+                NonZeroUsize::new(new_line.parse::<usize>().expect("Failed parsing into usize"))
+        }
+    }
+
+    amount
 }
