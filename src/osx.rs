ae3fc3a9ff0d64254e9183aea5a80795094015d5
Tomio <mail@tomio.fun>
2022-06-25 13:54:01 +0800

    chore: pointer

--- src/osx.rs
+++ src/osx.rs
@@ -2,19 +2,14 @@ use std::num::NonZeroUsize;
 use std::ptr;
 
 use mach2::kern_return::KERN_SUCCESS;
-use mach2::mach_init::mach_thread_self;
 use mach2::task::task_threads;
 use mach2::traps::mach_task_self;
 
 pub(crate) fn thread_amount() -> Option<NonZeroUsize> {
     let mut state = [0u32; 1296];
     let mut count: u32 = 0;
-    let rc = unsafe {
-        let task = mach_task_self();
-        let thread = mach_thread_self();
-
-        task_threads(task, &mut state as *mut *mut _, &mut count)
-    };
+    let rc =
+        unsafe { task_threads(mask_task_self(), &mut state as *mut *mut _, &mut count as *mut _) };
 
     if rc == KERN_SUCCESS {
         NonZeroUsize::new(usize::try_from(count).expect("Failed converting from u32 to usize"))
