6ab77b25f274378e019bb261179d7a07ddfcf468
jzeuzs <75403863+jzeuzs@users.noreply.github.com>
2025-11-06 02:58:31 +0800

    fix: osx

--- src/osx.rs
+++ src/osx.rs
@@ -8,8 +8,13 @@ use mach2::traps::mach_task_self;
 pub(crate) fn thread_amount() -> Option<NonZeroUsize> {
     let mut state = [0u32; 1296];
     let mut count: u32 = 0;
-    let rc =
-        unsafe { task_threads(mach_task_self(), &mut state as *mut *mut u32, &mut count as *mut _) };
+    let rc = unsafe {
+        task_threads(
+            mach_task_self(),
+            &mut state.as_mut_ptr() as *mut *mut u32,
+            &mut count as *mut _,
+        )
+    };
 
     if rc == KERN_SUCCESS {
         NonZeroUsize::new(usize::try_from(count).expect("Failed converting from u32 to usize"))
