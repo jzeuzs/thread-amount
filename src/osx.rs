0c5929d349aea4dbd3468034ce9720bcc938050e
jzeuzs <75403863+jzeuzs@users.noreply.github.com>
2025-11-22 00:53:30 +0800

    fix: import `mach_port_t` from `mach2::port`

--- src/osx.rs
+++ src/osx.rs
@@ -2,8 +2,9 @@ use std::num::NonZeroUsize;
 use std::ptr;
 
 use mach2::kern_return::KERN_SUCCESS;
-use mach2::mach_types::{mach_port_t, thread_act_array_t, vm_address_t, vm_size_t};
+use mach2::mach_types::{thread_act_array_t, vm_address_t, vm_size_t};
 use mach2::message::mach_msg_type_number_t;
+use mach2::port::mach_port_t;
 use mach2::task::task_threads;
 use mach2::traps::mach_task_self;
 use mach2::vm::mach_vm_deallocate;
