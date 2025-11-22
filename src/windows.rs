72063d3a38367b636c1a66b71651ba0774eb0db1
jzeuzs <75403863+jzeuzs@users.noreply.github.com>
2025-11-23 03:01:55 +0800

    feat: switch to `windows-sys`

--- src/windows.rs
+++ src/windows.rs
@@ -1,50 +1,46 @@
+use std::mem;
 use std::num::NonZeroUsize;
-use std::{mem, process, ptr};
 
-use field_offset::offset_of;
-use windows::Win32::Foundation::CloseHandle;
-use windows::Win32::System::Diagnostics::ToolHelp::{
+use windows_sys::Win32::Foundation::{CloseHandle, INVALID_HANDLE_VALUE};
+use windows_sys::Win32::System::Diagnostics::ToolHelp::{
     CreateToolhelp32Snapshot,
     TH32CS_SNAPTHREAD,
     THREADENTRY32,
     Thread32First,
     Thread32Next,
 };
+use windows_sys::Win32::System::Threading::GetCurrentProcessId;
 
 pub(crate) fn thread_amount() -> Option<NonZeroUsize> {
-    let mut amount: usize = 0;
-    let pid = process::id();
-
-    // https://devblogs.microsoft.com/oldnewthing/20060223-14/?p=32173
     unsafe {
-        let handle =
-            CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0).expect("Failed creating snapshot");
+        let pid = GetCurrentProcessId();
+        let handle = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
 
-        if !handle.is_invalid() {
-            let mut te = THREADENTRY32 {
-                dwSize: mem::size_of::<THREADENTRY32>()
-                    .try_into()
-                    .expect("Failed converting usize into u32"),
-                ..Default::default()
-            };
+        if handle == INVALID_HANDLE_VALUE {
+            return None;
+        }
 
-            if Thread32First(handle, ptr::addr_of_mut!(te)).is_ok() {
-                while Thread32Next(handle, ptr::addr_of_mut!(te)).is_ok() {
-                    if te.dwSize as usize
-                        >= (offset_of!(THREADENTRY32 => th32OwnerProcessID).get_byte_offset()
-                            + mem::size_of::<u32>())
-                        && te.th32OwnerProcessID == pid
-                    {
-                        amount += 1;
-                    }
+        let mut count = 0;
+
+        let mut te: THREADENTRY32 = mem::zeroed();
+        te.dwSize = mem::size_of::<THREADENTRY32>() as u32;
+
+        if Thread32First(handle, &mut te) != 0 {
+            loop {
+                if te.th32OwnerProcessID == pid {
+                    count += 1;
+                }
+
+                if Thread32Next(handle, &mut te) == 0 {
+                    break;
                 }
             }
         }
 
-        CloseHandle(handle).expect("Failed closing handle");
-    }
+        CloseHandle(handle);
 
-    NonZeroUsize::new(amount)
+        NonZeroUsize::new(count)
+    }
 }
 
 #[inline]
